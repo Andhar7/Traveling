d604b26f0df9552ca7609e7feeb7988e67ce4172
Andhar7 <70223638+Andhar7@users.noreply.github.com>
2020-12-29 17:25:21 +0100

    Initial Commit

---
+++ Traveling/TravelingApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelingApp.swift
+//  Traveling
+//
+//  Created by Andrej Kling on 29.12.20.
+//
+
+import SwiftUI
+
+@main
+struct TravelingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
